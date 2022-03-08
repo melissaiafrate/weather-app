e85030e3a2f213ce34f8ad9d589ee4c7e946f92f
melissaiafrate <98415279+melissaiafrate@users.noreply.github.com>
2022-03-07 22:00:07 -0500

    Updating JS for current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function showTemperature(response) {
+  let todayTempNumber = document.querySelector("#today-temp-number");
+  let forecastCity = document.querySelector("#current-city-forecast");
+  let description = document.querySelector("#weather-description");
+  let currentHumidity = document.querySelector("#humidity");
+  let currentWindSpeed = document.querySelector("#wind-speed");
+  todayTempNumber.innerHTML = Math.round(response.data.main.temp);
+  forecastCity.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].main;
+  currentHumidity.innerHTML = response.data.main.humidity;
+  currentWindSpeed.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "e4386934c81dcc4d977985af91d7aadd";
+let city = "Ocho Rios";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showTemperature);
